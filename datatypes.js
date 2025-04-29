26b58d91149080ffec0752df3b2b31d185ae70b6
G Thimmaraju <thimmaraju418@gmail.com>
2025-04-29 08:30:39 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,94 @@
+
+
+ samplevariabale = "10"
+ 
+ //typeof 
+
+ console.log(samplevariabale)
+
+ console.log(typeof(samplevariabale)) 
+
+ st1 = "raju"
+
+ console.log(typeof(st1)) 
+
+ st2 = 'Basavaraju'
+
+ console.log(typeof(st2)) 
+
+
+ st3 = `arjun`
+
+ 
+ console.log(typeof(st3)) 
+
+ //await pageXOffset.locator(string)
+
+	   sentence1 = "Ajun place 'Bangalore'"
+
+var stname 
+
+console.log(typeof(stname))
+
+st3name = undefined
+
+console.log(typeof(st3name))
+
+
+ sampleemptvar = null
+
+ console.log(typeof(sampleemptvar))
+
+//  In C 
+
+//   x = null  // Object 
+
+
+arr = ["Admin", "admin123", 8]  //arrays 
+
+console.log(typeof(arr[2])) //Object
+
+//elements 
+ 
+creds = {
+
+    username : "Admin",
+    password  : "admin123"
+}
+
+// Properties of Object
+
+
+// objectname = {
+
+//     propertyname : propertyvalue
+// }
+
+
+//  //Objects 
+
+//  Json 
+
+//  {
+//     "username" : "Admin",
+//     "password" : "admin123"
+ 
+//  }
+
+
+//  //object
+//  logindata = 
+//  {
+//     "username" : "Admin",
+//     "password" : "admin123"
+ 
+//  }
+
+car = {
+
+    geartype : "Manual",
+    color : "Blue",
+    weight : 1200,
+    model : 2025
+}
+//console.log(typeof(car))
---
+++ varibales.js
@@ -0,0 +1,107 @@
+// var x = 10
+
+// var y = 20 
+
+// z = x+y 
+
+// console.log(z)
+
+
+// //========================================
+
+// console.log("Raju")
+
+
+// stuname = "Raju"
+
+// 3 key words are available to define scope 
+
+//var , let  and const 
+
+//var stunname = "Raju"
+
+//let stuname = "Raju"
+
+//const stuname = "Raju"
+
+{
+
+
+
+
+
+    if(5>3){
+    
+        let greet = "HI"
+
+       console.log(greet)
+    }
+
+    //console.log(greet)
+
+}
+
+
+// var - variable global scope 
+
+// let and const - Blocked scope 
+
+var stuname = "Arjun"
+
+stuname = "Darshan"
+
+console.log(stuname)
+
+// reassign the different value is possible 
+
+if(true){
+
+   let  trainer = "Raju"
+
+   trainer = "Murali"
+
+   console.log(trainer)
+
+}
+
+
+
+// var - variable global scope 
+
+// let and const - Blocked scope 
+
+
+//reassign the different value is possible with var and let 
+////reassign the different value is not possible with const
+
+
+// const  ordernuber = "48569869"
+
+// const  ordernuber = "678954697"
+
+console.log(ordernuber)
+
+// var - variable global scope 
+
+// let and const - Blocked scope 
+
+
+//reassign the different value is possible with var and let 
+//reassign the different value is not possible with const
+
+// re - declaration same varibale is possible with var 
+
+// re - declaration same varibale is not possible with let and const 
+
+
+
+//var available from initial version of JS 
+
+//let and const  - from es6 version 
+
+
+var 
+let 
+// const 
+
+// var 
