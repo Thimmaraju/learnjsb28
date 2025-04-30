324a6ccad522a411ced766619a4b14a5c50fe5fa
G Thimmaraju <thimmaraju418@gmail.com>
2025-04-30 09:37:37 +0530

    loops

--- conditionalstatements.js
+++ conditionalstatements.js
@@ -1,1 +1,198 @@
-igkjetghiu4
+
+
+//condition - pass 
+
+if (2 === "2") {
+
+    console.log("Raju")
+
+    console.log("Arjun")
+
+    console.log("Deekshit")
+
+
+}
+
+
+
+//====================
+
+// condition - Pass - block 1 
+
+//            fail  - block 2 
+
+if (true) {
+
+
+    console.log("Block 1")
+}
+else {
+
+
+    console.log("Blockk 2")
+}
+
+
+
+//condtion 1 - pass 
+
+//  Block1 
+
+//  // condition 2 - pass
+
+//  Block2 
+
+
+if (false) {
+
+    console.log(" 1st block ")
+}
+else if (false) {
+
+    console.log(" 2nd block ")
+}
+
+else if (false) {
+
+    console.log(" 3rd block ")
+}
+
+
+
+//  if(){
+
+
+
+
+//  }
+
+//  else if (){
+
+
+//  }
+
+//  else if (){
+
+
+//  }
+//  else{
+
+
+//  }
+
+// var x = "Tuesday"
+
+// switch (x) {
+//     case "Sunday":
+
+//         console.log("X value is sunday")
+//         break;
+//     case "Monday":
+
+//         console.log("X value is Monday")
+//         break;
+
+//     case "Tuesday":
+
+//         console.log("X value is Tuesday")
+//         break;
+
+//     case "Wednesday":
+
+//         console.log("X value is Wednesday")
+//         break;
+
+//     default:
+//         console.log("X value is not  a day")
+
+//         break;
+// }
+
+
+switch (4) {
+    case 1:
+        {
+
+            console.log("Block 1")
+        }
+        break;
+
+    case 2:
+        {
+
+            console.log("Block 2")
+        }
+        break;
+
+    case 3:
+        {
+
+            console.log("Block 3")
+        }
+        break;
+    case 4:
+        {
+
+            console.log("Block 4")
+        }
+       break;
+
+    case 5:
+        {
+
+            console.log("Block 5")
+        }
+
+
+    default:
+
+    {
+
+        console.log("Defaulat 5")
+    }
+        break;
+}
+
+if (){
+
+
+}
+
+or 
+
+if(){
+
+
+}
+else{
+
+
+}
+
+or 
+
+if(){
+
+
+}
+
+else if (){
+
+
+}
+
+else{
+
+
+}
+
+or 
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
---
+++ loops.js
@@ -0,0 +1,94 @@
+
+
+// {
+//     console.log("Raju")
+// }
+
+// for
+// while 
+
+// do while 
+
+// for( initialization ; condition ; increment/decrement){
+
+
+// }
+
+
+// for(let i=10 ; i>0; i--){
+
+//     console.log(i)
+//     console.log("Raju")
+//     console.log("Deekshit")
+//     console.log("Darshan")
+//     console.log("Arjun")
+
+// }
+
+// while (condition) {
+
+// }
+
+
+// i = 11
+
+// while (i>10){
+
+//     console.log(i)
+//     console.log("Raju")
+//     console.log("Deekshit")
+
+//     i++
+// }
+
+
+// do{
+
+
+// }while (condition) {
+
+// }
+
+
+i = 11
+
+do {
+
+    console.log(i)
+    console.log("Raju")
+    console.log("Deekshit")
+
+    i++
+} while (i < 10)
+
+//for and while 
+
+//if the condition id failing then it wont execute even single 
+
+// do while 
+
+// doent matter pass or fail  at least one time it will execute the block of code 
+
+//es6
+
+// car = {
+
+
+// }
+
+// for in
+
+// arr = []
+
+// for of 
+
+// array.forEach(element => {
+    
+// });
+
+// arr = ["raju", "arjun", "xyz"]
+
+// test {
+
+
+// }
